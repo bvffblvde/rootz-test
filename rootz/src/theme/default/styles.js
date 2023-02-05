b29c37fb08ca4944f04cb8be0c144996bdea228b
bvffblvde <krakenitsmydog@gmail.com>
2023-02-05 16:02:49 +0200

    clear not used code, create theme directory && first step for building layout

---
+++ rootz/src/theme/default/styles.js
@@ -0,0 +1,15 @@
+const colors = {
+    primary: '#000',
+    secondary: '#fff',
+};
+
+const sizes = {
+    tablet: 768,
+    desktop: 1024,
+};
+
+const fonts = {
+    avenir: 'Avenir',
+};
+
+export { colors, sizes, fonts };
